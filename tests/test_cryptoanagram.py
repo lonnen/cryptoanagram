cd43fc02f8921b8c44cd812a611e8d38afbff0e8
lonnen <chris.lonnen@gmail.com>
2020-04-02 00:32:26 -0700

    add the basic test skeleton

---
+++ tests/test_cryptoanagram.py
@@ -0,0 +1,13 @@
+from .context import cryptoanagram
+
+import unittest
+
+class TestSuite(unittest.TestCase):
+    """the easiest person to fool is yourself"""
+
+    def test_truth(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
