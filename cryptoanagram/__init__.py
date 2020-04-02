8dea7f2f031e275bbb1f679cbf65f429245a4f35
lonnen <chris.lonnen@gmail.com>
2020-04-02 01:37:14 -0700

    fixup for the flake8 lints

--- cryptoanagram/__init__.py
+++ cryptoanagram/__init__.py
@@ -1,5 +1,3 @@
-from .utils import datadir, load_words
-
 __author__ = "Lonnen"
 __email__ = "chris.lonnen@gmail.com"
 
@@ -10,6 +8,6 @@ __version__ = "0.1.0"
 
 
 QWANTZLE_LETTERS = (
-    "ttttttttttttooooooooooeeeeeeeeaaaaaaallllllnnnnnnuuuuuuiiiiisssssdddddhhhhh"
-    + "yyyyyIIrrrfffbbwwkcmvg:,!!"
+    "ttttttttttttooooooooooeeeeeeeeaaaaaaallllllnnnnnnuuuuuuiiiiisssssddddd"
+    + "hhhhhyyyyyIIrrrfffbbwwkcmvg:,!!"
 )
