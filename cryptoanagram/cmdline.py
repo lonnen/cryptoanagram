e30131e8373eef4d1d162bf3a0b30d578500e3d0
lonnen <chris.lonnen@gmail.com>
2020-05-21 04:13:38 +0000

    planning future stats to read out

--- cryptoanagram/cmdline.py
+++ cryptoanagram/cmdline.py
@@ -6,3 +6,6 @@ def run():
     s = sys.argv[1:]
     c = Cryptoanagram(" ".join(s))
     print(c.ordered)
+    # unordered = Multiset(unordered)
+    # ordered = ordered
+    # dictionary = list(filter(self._filter, dictionary))
