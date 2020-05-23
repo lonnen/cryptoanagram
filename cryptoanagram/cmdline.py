822bdc86d8f17cb00b7e878a70be60a2ed695143
lonnen <chris.lonnen@gmail.com>
2020-05-23 18:42:14 +0000

    add formatting for letters remaining

--- cryptoanagram/cmdline.py
+++ cryptoanagram/cmdline.py
@@ -6,8 +6,10 @@ def run():
     s = sys.argv[1:]
     submitted = " ".join(s)
     c = Cryptoanagram(submitted)
-    print("\nCandidate: ", submitted, "\n")
-    print(c.ordered)
-    print(c.unordered)
+    print("\n")
+    print("Candidate: ", submitted, "\n")
+
+    print("\n")
+    print("Remaining: ", ''.join(sorted(c.unordered)), "\n")
     for w in c.dictionary:
         print(w.ordered)
