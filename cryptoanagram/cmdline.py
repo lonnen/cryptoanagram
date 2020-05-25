0f8fe5591d6039d79a10cd2c78bad15aeeca2802
lonnen <chris.lonnen@gmail.com>
2020-05-25 00:19:14 +0000

    something is wrong with the word object
    printing out word I'm only seeing the first letter. This unrolls the
    process of creating the final cryptoanagram object by building it up
    one word addition at a time

--- cryptoanagram/cmdline.py
+++ cryptoanagram/cmdline.py
@@ -5,11 +5,15 @@ from . import Cryptoanagram
 def run():
     s = sys.argv[1:]
     submitted = " ".join(s)
-    c = Cryptoanagram(submitted)
+    c = Cryptoanagram("")
+    for word in s:
+        c.push(word)
     print("\n")
     print("Candidate: ", submitted, "\n")
 
     print("\n")
     print("Remaining: ", ''.join(sorted(c.unordered)), "\n")
-    for w in c.dictionary:
+
+    print("Next Words: \n")
+    for w in c._dictionary:
         print(w.ordered)
