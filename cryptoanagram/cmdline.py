d550b402190961a9233baa608c717cd2cecf1c8d
lonnen <chris.lonnen@gmail.com>
2020-05-26 00:56:43 +0000

    additional word formatting

--- cryptoanagram/cmdline.py
+++ cryptoanagram/cmdline.py
@@ -6,14 +6,13 @@ def run():
     s = sys.argv[1:]
     submitted = " ".join(s)
     c = Cryptoanagram("")
-    for word in s:
-        c.push(word)
-    print("\n")
-    print("Candidate: ", submitted, "\n")
 
     print("\n")
-    print("Remaining: ", ''.join(sorted(c.unordered)), "\n")
+    print("Candidate: ", submitted)
+    print("\n")
+    print("Remaining: ", ''.join(sorted(c.unordered)))
 
     print("Next Words: \n")
-    for w in c._dictionary:
-        print(w.ordered)
+    for w in c.dictionary:
+        #print(w.ordered)
+        pass
