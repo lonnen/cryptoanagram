74a45e8be424ea952976a315809460a48a83b7c9
lonnen <chris.lonnen@gmail.com>
2021-05-21 00:50:56 -0700

    I would have to look this line of jq up every time otherwise

---
+++ scripts/extract-text.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+usage() {
+    cat <<HELP
+
+    Usage: $(basename "$0") raw_dino_comics.json [options] > dino_comics.txt
+
+    Options:
+
+      -h, --help              This message
+
+    This transforms the output of github.com/lonnen/dinocomics-scraper by
+    extracting just the text of the comics and discarding the json formatting,
+    source material, and titles. The output is then sent to stdout
+
+HELP
+}
+
+
+if [ "$#" < 1]; then
+    usage
+fi
+
+case $1 in
+    -h|--help)
+        usage
+        exit 0
+        ;;
+    *)
+        cat $1 | jq '.[].text' --raw-output
+esac
