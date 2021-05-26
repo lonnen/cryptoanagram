ead759df2b8ea432e34bd0379553547a32750a6e
lonnen <chris.lonnen@gmail.com>
2021-05-26 01:11:13 -0700

    ComicsParser - handle all the CLI args and file reading

--- scripts/comics-parser/ComicsParser.hs
+++ scripts/comics-parser/ComicsParser.hs
@@ -1,5 +1,21 @@
 #!/usr/bin/env stack
 
-main :: IO ()
-main = do
-    print "Hello"
+import System.Environment (getArgs)
+import System.Exit ( ExitCode(ExitSuccess), ExitCode(ExitFailure), exitWith )
+import Text.ParserCombinators.ReadP
+
+main = getArgs >>= parse >>= putStr . clean
+
+# replace this with a meaningful parser
+clean = unlines . lines
+
+isColon :: Char -> Bool
+isColon char = char == ':'
+
+parse ["-h"] = usage >> exit
+parse [] = getContents
+parse fs = concat `fmap` mapM readFile fs
+
+usage = putStrLn "comicsParser file"
+exit  = exitWith ExitSuccess
+die   = exitWith (ExitFailure 1)
