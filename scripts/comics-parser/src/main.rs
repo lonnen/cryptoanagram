5a688b86e59afbef591cc1db48b0bb45b7a93eae
lonnen <chris.lonnen@gmail.com>
2021-05-30 01:47:32 -0700

    oxidize it

--- scripts/comics-parser/src/main.rs
+++ scripts/comics-parser/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    input: std::path::PathBuf,
+}
+
+
+fn main() -> io::Result<()> {
+    let args = Cli::from_args();
+
+    let file = File::open(args.input)?;
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }
