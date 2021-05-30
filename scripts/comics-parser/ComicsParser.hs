c03facf39d905a2e4d4bdb30909cb00bb18f811f
lonnen <chris.lonnen@gmail.com>
2021-05-29 20:38:09 -0700

    ughhhhh I think I'm going to abandon parsec

--- scripts/comics-parser/ComicsParser.hs
+++ scripts/comics-parser/ComicsParser.hs
@@ -1,5 +1,7 @@
 #!/usr/bin/env stack
 
+{-# LANGUAGE FlexibleContexts #-}
+import Control.Applicative hiding ((<|>))
 import System.Environment (getArgs)
 import System.Exit ( ExitCode(ExitFailure), exitSuccess, exitWith )
 import Text.ParserCombinators.Parsec
@@ -7,7 +9,33 @@ import Text.ParserCombinators.Parsec
 main = getArgs >>= parseArgs >>= putStr . clean
 
 --replace this with a meaningful parser
-clean = unlines . lines
+clean :: String -> String
+clean = unlines . parse (many1 panel) "ComicsParser"
+
+paddedChar :: Char
+paddedChar c = char c <* spaces
+
+data Panel = Panel String String deriving Show
+
+panel :: Parser Panel
+panel = do
+    speaker <- many1 letter <* paddedChar ':'
+    quip <- many1 (noneOf "\n") <* paddedChar '\n'
+
+    tokens <- oneOf "!\"#$%&\'()*+,-./:;<=>?@[]^_`{|}~"
+
+    return $ Panel speaker quip
+
+data ComicWord = String
+
+prettyPrintWord :: ComicWord -> String
+prettyPrintWord = show
+
+comicWord :: Parser ComicWord
+comicWord = do
+    s <- noneOf "!\"#$%&\'()*+,-./:;<=>?@[]^_`{|}~"
+
+    return $ ComicWord
 
 parseArgs ["-h"] = usage >> exit
 parseArgs [] = getContents
