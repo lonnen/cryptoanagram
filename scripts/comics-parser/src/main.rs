17a69811b991c96846959846d1f0005d5b6aa8c1
lonnen <chris.lonnen@gmail.com>
2021-06-04 00:15:23 -0700

    prefer raw unwrap to a bad exception message

--- scripts/comics-parser/src/main.rs
+++ scripts/comics-parser/src/main.rs
@@ -18,7 +18,7 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     for line in reader.lines().map(|l| l.unwrap()) {
-        comics_parser::lex(&line).expect("trouble")
+        comics_parser::lex(&line).unwrap()
             .iter()
             .for_each(|p| println!("{:?}", p));
     }
