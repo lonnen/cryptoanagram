7312d4eabb7d72af4a1ed6de0d6d6d87caeb64a4
lonnen <chris.lonnen@gmail.com>
2021-06-06 02:30:05 -0700

    parser progress

--- scripts/comics-parser/src/lib.rs
+++ scripts/comics-parser/src/lib.rs
@@ -1,6 +1,15 @@
 use nom::{
     IResult,
-    bytes::complete::take_until,
+    bytes::complete::{
+        take_until,
+        tag,
+    },
+    character::is_space,
+    multi::fold_many0,
+    sequence::{
+        separated_pair,
+        terminated,
+    },
 };
 
 #[derive(Debug, Clone)]
@@ -11,19 +20,36 @@ pub enum LexItem {
     EOL
 }
 
-pub fn speaker(input : &str) -> IResult<&str, &str> {
-    take_until(":")(input)
+pub fn line(input: &str) -> IResult<&str, (&str, &str)> {
+    separated_pair(
+        take_until(":"),
+        tag(":"),
+        terminated(take_until("\n"), tag("\n"))
+    )(input)
 }
 
-// pub fn eol(input: &str) -> IResult<&str, LexItem::EOL> {
-//     take_while(is_newline)(input);
-// }
+pub fn is_space_or_punctuation(chr: u8) -> bool {
+    is_space(chr) || "!\"#$%&()*+,-./:;<=>?@[]^_`{|}~".contains(chr as char)
+}
+
+pub fn words(input: &str) -> IResult<&str, Vec<&str>> {
+    fold_many0(
+        take_until(
+            is_space_or_punctuation
+        ),
+        Vec::new(),
+        |mut acc: Vec<_>, item| {
+            acc.push(item);
+            acc
+        }
+    )(input)
+}
 
 pub fn lex(input: &str) -> Result<Vec<LexItem>, String> {
     let mut result = Vec::new();
     let mut word = Vec::new();
 
-    let (input, speaker) = take_until(":")(input);
+    // let (input, speaker) = take_until(":")(input);
 
     let mut it = input.chars().peekable();
     while let Some(&c) = it.peek() {
@@ -63,4 +89,10 @@ pub fn lex(input: &str) -> Result<Vec<LexItem>, String> {
     }
 
     Ok(result)
+}
+
+#[test]
+fn test_line() {
+    assert_eq!(line("professor: I can't believe it\n"), Ok(("", ("professor", " I can\'t believe it"))));
+    assert_eq!(line("professor: believe me: it's bad\n"), Ok(("", ("professor", " believe me: it's bad"))))
 }
