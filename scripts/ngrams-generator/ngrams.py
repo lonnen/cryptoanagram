7fc9ef9c5cf43d560ba40a4bc865b122b3d0ae02
lonnen <chris.lonnen@gmail.com>
2021-06-18 00:30:11 -0700

    ngrams: more sensible args

--- scripts/ngrams-generator/ngrams.py
+++ scripts/ngrams-generator/ngrams.py
@@ -5,6 +5,9 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="fuuu up a text file")
-    parser.add_argument('-n', help="how many words to a gram")
-    parser.add_argument('-c', '--count', help="should ngrams be published with occurance counts")
-    print('stuff')
+    parser.add_argument('-n', help="how many words to a gram", type=int)
+    parser.add_argument('-c', '--count', help="should ngrams be published with occurance counts",
+        action='store_true')
+    args = parser.parse_args()
+
+    print(args)
