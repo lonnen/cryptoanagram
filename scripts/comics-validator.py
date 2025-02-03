12e3a0c80516c2e01cb414d2e5e7a66b81fd1490
lonnen <chris.lonnen@gmail.com>
2025-02-03 01:09:50 -0800

    remove all duplicated entries, add dupe detector to the validator

--- scripts/comics-validator.py
+++ scripts/comics-validator.py
@@ -8,13 +8,25 @@ USAGE = """Usage: python comics-validator.py <filename>
     filename - the output of comics-parser.py
 """
 
-
 def check_comics(filename):
+    comic_id_pointer = 1
     try:
         with open(filename, "r") as file:
             comics = json.load(file)
             for comic in comics:
                 id, _title, panels, metadata, _url = comic.values()
+                if id > comic_id_pointer:
+                    print(f"Comic {comic_id_pointer} is missing",
+                        file=sys.stderr
+                    )
+                    comic_id_pointer=id
+                elif id < comic_id_pointer:
+                    print(
+                        f"Comic {id} is duplicated",
+                        file=sys.stderr
+                    )
+                    comic_id_pointer=id
+                comic_id_pointer += 1
                 if len(panels) != 6 and ("irregular" not in metadata):
                     print(
                         f"{id} has irregular panel count: {len(panels)}",
